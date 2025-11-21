792bdeae0d497034828ec013b7a9e69315a1b142
vpatel1980 <35194214+vpatel1980@users.noreply.github.com>
2025-11-21 11:41:35 +0000

    Update streamlit_app.py
    Added JSON into a Dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,7 @@ if ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
+st_df=st.dataframe(data=smoothiefroot_response.json(),user_container_width=true)
+
+
+
